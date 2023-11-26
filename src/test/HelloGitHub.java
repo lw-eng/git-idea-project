acca194908010f08017c413253d1fce9d6ba94ba
Layne <Layne@atliwen.com>
2023-11-26 20:19:39 +0800

    second-commit

--- src/test/HelloGitHub.java
+++ src/test/HelloGitHub.java
@@ -4,5 +4,6 @@ public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("add source");
+        System.out.println("hello github2");
     }
 }
