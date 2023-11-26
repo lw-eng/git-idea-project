8768a5286459bfa2c980a9e136cd52d1d7d4f26f
Layne <Layne@atliwen.com>
2023-11-26 18:33:09 +0800

    update source commit

--- src/test/HelloGitHub.java
+++ src/test/HelloGitHub.java
@@ -3,5 +3,6 @@ package test;
 public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
+        System.out.println("add source");
     }
 }
